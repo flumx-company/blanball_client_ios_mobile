9c2a4461c9a0dd48cdeac5535e2eaeace3686868
Misha Deneka <mishadeneka@gmail.com>
2023-02-27 14:57:18 +0200

    created project

---
+++ Blanball/Blanball/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Blanball
+//
+//  Created by Misha Deneka on 27.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController")
+        
+        return true
+    }
+}
+
