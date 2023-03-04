7154874ab6a0ef138748ac66e913de105bf602a1
Misha <33558690+MrLizarx@users.noreply.github.com>
2023-03-04 17:33:41 +0200

    Feature/swiftgen
    * added colors
    
    * fixed apiHost
    
    * swiftgen script
    
    * fixed "missing scene configuration" alert

---
+++ Blanball/Common/Base/Entities/App/EnviromentVars.swift
@@ -0,0 +1,45 @@
+//
+//  EnviromentVars.swift
+//  Blanball
+//
+//  Created by Misha Deneka on 03.03.2023.
+//
+
+import Foundation
+
+private struct EnvironmentKeys {
+    static let apiBaseURL = "ApiBaseURL"
+    static let configuration = "Configuration"
+    static let scheme = "Scheme"
+    static let isInDebug = "IsInDebug"
+}
+
+struct EnvironmentVars {
+    static let isInDebug: Bool = {
+        let value = Bundle.main.infoDictionary?[EnvironmentKeys.isInDebug] as? String ?? "NO"
+        return value == "YES"
+    }()
+
+    static let configuration: String = {
+        let value = Bundle.main.infoDictionary?[EnvironmentKeys.configuration] as? String ?? "test"
+        return value
+    }()
+    
+    static let scheme: String = {
+        let value = Bundle.main.infoDictionary?[EnvironmentKeys.scheme] as? String ?? "test"
+            return value
+    }()
+    
+    static let baseApiHost: String = {
+        let value = Bundle.main.infoDictionary?[EnvironmentKeys.apiBaseURL] as? String ?? ""
+        return value
+    }()
+
+    static let webSocketApiHost: String = {
+        return "ws://\(baseApiHost)/"
+    }()
+    
+    static let apiHost: String = {
+        return "http://\(baseApiHost)/api/"
+    }()
+}
