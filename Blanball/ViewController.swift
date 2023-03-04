f362fddad8b02d5fd28e1a8ad69efe26eab6b1c9
Misha Deneka <mishadeneka@gmail.com>
2023-03-04 14:48:39 +0200

    swiftgen script

---
+++ Blanball/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Blanball
+//
+//  Created by Misha Deneka on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
